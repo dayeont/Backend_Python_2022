2fc6b9176eb449d2299e07843c1f6a0fc114a697
Gazizova.DA <Gazizova.DA@gazprom-neft.ru>
2022-12-10 23:27:37 +0300

    add read_data

--- Ex1/nearest_zero.py
+++ Ex1/nearest_zero.py
@@ -1,3 +1,10 @@
+def read_data() -> list:
+    s = input('Enter elements of a list separated by space ')
+    print("\n")
+    s = s.split()
+    return s
+
+
 def nearest_zero(s: list) -> str:
     ind = []
     for i, x in enumerate(s):
@@ -23,12 +30,14 @@ def nearest_zero(s: list) -> str:
     return ' '.join(map(str, result))
 
 
-def main():
-    s = input('Enter elements of a list separated by space ')
-    print("\n")
-    s = s.split()
-    result = nearest_zero(s)
+def print_result(result: str):
     print(result)
+
+
+def main():
+    s = read_data()
+    result = nearest_zero(s)
+    print_result(result)
 
 
 if __name__ == "__main__":
